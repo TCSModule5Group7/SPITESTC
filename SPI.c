cc6af5972127ae2d3f434711548893b0e0d08593
killera40 <arwin.sleutjes@gmail.com>
2017-11-13 11:45:11 +0100

    Last Commit

--- SPI.c
+++ SPI.c
@@ -4,31 +4,42 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define MOSI 10
-#define MISO 9
-#define SCLK 11
-#define CE_ZERO 8
-
-#define BCM2837_PERIPHERAL_BASE 0x3F000000
+#define BCM2837_PERIPHERAL_BASE 0x3F000000 //
 #define GPIO_BASE (BCM2837_PERIPHERAL_BASE + 0x00200000)
+
+#define PIN_OUT 3
+#define PIN_CLOCK 4
+#define MODE_CLOCK 4
+#define CONTROL_CLOCK (BCM2837_PERIPHERAL_BASE + 0x00101070)
+#define DIV_CLOCK (BCM2837_PERIPHERAL_BASE + 0x00101070)
 
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
 
 #define GPIO_INPUT(pin) *(gpio + ((pin) / 10)) &= ~(7 << (((pin) % 10) * 3))
 #define GPIO_OUTPUT(pin) *(gpio + ((pin) / 10)) |=  (1 << (((pin) % 10) * 3)) // Always use GPIO_INPUT first.
+#define GPIO_ALTERNATE_MODE(pin, mode) *(gpio + (((pin) / 10))) |= (((mode) <= 3 ? (mode) + 4 : (mode) == 4 ? 3 : 2) << (((pin) % 10) * 3))
+//                        ||||||||             ||    |||||
+#define GPIO_CLOCK_MODE 0b01011010000000000000000000000000
+//                        5   a   |  unused   |++ - <=>>>>
+// + MASH
+// - BUSY
+// < KILL
+// = ENAB
+// > SRC
 
-#define GPIO_HIGH(pin) *(gpio + 7) |= 1 << pin
-#define GPIO_LOW(pin) *(gpio + 10) |= 1 << pin
+#define GPIO_CLOCK_DIV 0b00000000 00000000 00000000 00000000
 
-#define GPIO_GET(pin) (*(gpio + 13) & (1 << (pin))) >> (pin)
+#define GPIO_HIGH(pin) *(gpio + 7) |= 1 << (pin)
+#define GPIO_LOW(pin) *(gpio + 10) |= 1 << (pin)
+
+#define GPIO_GET(pin) *(gpio + 13) >> (pin) & 1
 
 // #define GPIO_PULL *(gpio+37) // Pull up/pull down
 // #define GPIO_PULLCLK0 *(gpio+38) // Pull up/pull down clock
 
 
 int main(int argc, char **argv) {
-    unsigned int pin = 3;
     unsigned int interval = 1;
     unsigned int repetitions = 10;
 
@@ -44,14 +55,16 @@ int main(int argc, char **argv) {
         printf("Error mapping gpio memory (don't forget to prepend 'sudo')");
         exit(-1);
     }
+    GPIO_INPUT(PIN_CLOCK); // Always use GPIO_INPUT first.
+    GPIO_OUTPUT(PIN_CLOCK);
+    GPIO_ALTERNATE_MODE(PIN_CLOCK, MODE_CLOCK);
 
-    GPIO_INPUT(pin);
-    GPIO_OUTPUT(pin); // Always use GPIO_INPUT first.
-    GPIO_LOW(pin); // Start at LOW
+
+    GPIO_LOW(PIN_CLOCK); // Start at LOW
     for (unsigned int i = 0; i < repetitions; i++) {
-        GPIO_HIGH(pin);
+        GPIO_HIGH(PIN_OUT);
         sleep(interval);
-        GPIO_LOW(pin);
+        GPIO_LOW(PIN_OUT);
         sleep(interval);
     }
     return 0;
